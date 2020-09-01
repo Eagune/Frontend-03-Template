24a70e6efff7a3a8a2ed24db84fc881d8cb21a8d
dphuyijin <dphuyijin@dfl.com.cn>
2020-09-01 08:38:39 +0800

    week02

---
+++ week03/client.js
@@ -0,0 +1,142 @@
+const net = require('net');
+
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = null;
+  }
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i));
+    }
+  }
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      } else {
+        this.statusLine += char;
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WAITING_HEADER_BLOCK_END;
+      } else {
+        this.headerName += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = '';
+        this.headerValue = '';
+      } else {
+        this.headerValue += char;
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    } else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_BODY;
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      console.log(char);
+    }
+  }
+}
+
+class Request {
+  constructor(option) {
+    this.method = option.method || 'GET';
+    this.host = option.host;
+    this.port = option.port || 80;
+    this.path = option.path || '/';
+    this.body = option.body || {};
+    this.headers = option.headers || {};
+    if (!this.headers['Content-Type']) {
+      this.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    }
+    if (this.headers['Content-Type'] === 'application/json') {
+      this.bodyText = qs.stringify(this.body);
+    } else if (this.headers['Content-Type'] === 'application/x-www-form-urlencoded') {
+      this.bodyText = Object.keys(this.body).map(key => `${key}=${encodeURIComponent(this.body[key])}`).join('&');
+    }
+    this.headers['Content-length'] = this.bodyText.length;
+  }
+
+  send(conntection) {
+    return new Promise((resolve, reject) => {
+      const parser = new ResponseParser;
+      if (conntection) {
+        conntection.write(this.toString());
+      } else {
+        conntection = net.createConnection({
+          host: this.host,
+          port: this.port,
+        }, () => {
+          conntection.write(this.toString());
+        });
+        conntection.on('data', (data) => {
+          parser.receive(data.toString());
+          if (parser.isfinished) {
+            resolve(parser.response);
+            conntection.end();
+          }
+        });
+        conntection.on('error', (err) => {
+          reject(err);
+          conntection.end();
+        });
+      }
+    });
+  }
+
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r
+${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join('\r\n')}\r
+\r
+${this.bodyText}`;
+  }
+}
+
+void async function() {
+  let request = new Request({
+    method: 'POST',
+    host: '127.0.0.1',
+    port: '8088',
+    path: '/',
+    headers: {
+      ["X-FOO2"]: "customed",
+    },
+    body: {
+      name: 'winter',
+    }
+  });
+
+  let response = await request.send();
+  console.log(response);
+}();
